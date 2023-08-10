f8182fe5ec86a01eae759aa55889081c946853bf
jose-prieto-dlpx <jose.prieto@delphix.com>
2023-08-10 17:27:26 +0200

    Modified restore process to use the number of channels defined by the user. Added startup force mount to pre-snapshot to ensure no dead processes are there during the snapshot

--- src/resources/ds_preSnapshot.ps1
+++ src/resources/ds_preSnapshot.ps1
@@ -24,6 +24,9 @@ $svc_status = check_srvc_status $oraUnq
 if ($svc_status -eq "Running"){
     $ccf_file_old = "$virtMnt\$oraSrc\ccf_old.sql"
     $ccf_file_new = "$virtMnt\$oraSrc\CCF.SQL"
+    log "Restarting database to kill any zombie processes"
+    startup_force_mount
+    
     log "Checking for the existence of $ccf_file_new and moving to $ccf_file_old if necessary"
     
     if ((Test-Path "$virtMnt\$oraSrc\CCF.SQL")) {
